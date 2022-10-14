4cf42991a5359499a09043955a788e4ae72d74b0
Wone11 <gelayegeremew@gmail.com>
2022-10-14 16:40:36 +0300

    multiplications added

--- add.js
+++ add.js
@@ -6,6 +6,12 @@ function Sum(numberOne,numberTwo){
     return numberOne + numberTwo
 }
 
-
+/**
+ * Multplying a Number
+ */
+function Multply(one,two){
+    return one * two
+}
 //call the function and test
 console.log('Sum : ' + Sum(100,100))
+console.log('Multiply : ' + Multply(100,100))
