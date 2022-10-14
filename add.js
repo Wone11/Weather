999cbb4ee7f4e3e46c4b6fa702f69ead4f36998e
Wone11 <gelayegeremew@gmail.com>
2022-10-14 16:32:58 +0300

    subtract function added

--- add.js
+++ add.js
@@ -6,6 +6,13 @@ function Sum(numberOne,numberTwo){
     return numberOne + numberTwo
 }
 
+/**
+ * Subtract Functiokn
+ */
+function Subtract(one,two){
+    return one - two
+}
 
 //call the function and test
 console.log('Sum : ' + Sum(100,100))
+console.log('Subtract : ' + Subtract(100,60))
